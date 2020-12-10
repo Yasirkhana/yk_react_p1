1edca551d331600b1b0500ec02479e0f4713e7d6
Yasir Ahmad <khan10055yasir@gmail.com>
2020-12-11 01:39:44 +0500

    updated name

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <br/>
-        UPGRADED BY YASIR KHANA.
+         YASIR KHANA.
         <a
           className="App-link"
           href="https://reactjs.org"
