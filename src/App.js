facc48a4dd280a6c7be19ebcba136935b0664742
Yasir Ahmad <khan10055yasir@gmail.com>
2020-12-11 01:34:29 +0500

    updated
    updated simplily

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br/>
+        UPGRADED BY YASIR KHANA.
         <a
           className="App-link"
           href="https://reactjs.org"
